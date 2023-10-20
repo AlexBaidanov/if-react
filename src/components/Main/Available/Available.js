18f6fbe41ab8f81ccbf1338b544eecea9f48a14e
AlexBaidanov <alexbaidanov95@gmail.com>
2023-10-20 15:13:39 +0300

    add slice to hotels array

--- src/components/Main/Available/Available.js
+++ src/components/Main/Available/Available.js
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 
 export const Available = ({ className, hotels }) => (
   <div className={classNames('description', className)}>
-    {hotels.map((item) => (
+    {hotels.slice(0, 4).map((item) => (
       <figure key={item.id} className="homes__item _mobile">
         <img className="homes__image" src={item.imageUrl} alt={item.name} />
         <figcaption className="homes__description">{item.name}</figcaption>
